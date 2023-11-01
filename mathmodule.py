a7387c30ea0320efe24177f609b6f847e67593e2
Amit Verma <v99amit@gmail.com>
2023-11-01 19:47:34 +0530

    Commit

---
+++ mathmodule.py
@@ -0,0 +1,21 @@
+'''
+import math
+print(math.sqrt(16))
+print(math.pi)
+print(math.floor(3.9))
+print(math.ceil(3.9))
+
+import math as p
+print(p.sqrt(25))
+print(p.pi)
+
+'''
+from math import *
+r=int(input('Enter Radius : ')
+print('Area of circle : ', pi*r**2)
+print('Area of circle : ',pi*pow(r,2))
+
+
+
+
+
