4e48940567faee4779648950a83e48947f501bb5
Amit Verma <v99amit@gmail.com>
2023-11-03 20:33:21 +0530

    Commit

---
+++ Equality.py
@@ -0,0 +1,16 @@
+'''
+print(10==20)
+print(10!=20)
+print(100 = True)
+print(False == False)
+print(True == True)
+print('Ranjan'=='Ranjan')
+'''
+a=5,
+b=6
+if(a<b):
+        print("a is less than b ")
+        print("Hi Amit")
+        print("Hi Swarupam")
+        print("Hi Sagar")
+
