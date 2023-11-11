214b9913e7ff47556423cf49006da24907e4c0b2
Amit Verma <v99amit@gmail.com>
2023-11-11 22:18:15 +0530

    Commit

---
+++ Relation1.py
@@ -0,0 +1,51 @@
+#Relational operator
+'''
+a=10
+b=20
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)
+
+s1='a'
+s2='b'
+print(ord('a'))
+print(ord('A'))
+print(chr(67))
+
+s1='amit'
+s2='ankit'
+print(s1<s2)
+print(s1<=s2)
+print(s1>s2)
+print(s1>=s2)
+s1='amit'
+s2='Amit'
+print(s1<s2)
+print(s1<=s2)
+print(s1>s2)
+print(s1>=s2)
+
+print(True>False)#T
+print(True>=False)#T
+print(True<False)#F
+print(True<=False)#F
+
+print(10<'amit') #Type error
+
+a=10
+b=20
+if a>b :
+	print('a is greater than b')
+else :
+	print('a is not greater than b')
+'''
+print(10<20<30) #T
+print(10<20<30<40) #T
+print(10<20<30<40>50) #F
+
+
+
+
+
+
