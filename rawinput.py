2d7fb4c27547e8cbf35f2350dd367fd89f47c8b4
Amit Verma <v99amit@gmail.com>
2023-11-14 19:41:01 +0530

    Commit

---
+++ rawinput.py
@@ -0,0 +1,11 @@
+'''
+x= raw_input("Enter some value : ")
+y=int(x)               //raw input provide only string
+print(type(x))
+'''
+#x=input(Enter some value) //only for python 2.0
+#print(type(x))
+
+x=input('Enter some thing') #input fn- only provide string
+print(type(x))
+print(x)
