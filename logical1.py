253dce9b8f5edc0266726d4528502b200a42a8f6
Amit Verma <v99amit@gmail.com>
2023-11-16 22:04:13 +0530

    Commit

---
+++ logical1.py
@@ -0,0 +1,20 @@
+#logical operator
+ #and,or,not
+   #and(both are true)
+
+print('True' and 'True')
+print('True' and 'False')
+print('False' and 'True')
+print('False' and 'False')
+
+#or(return true if one arguent is true)
+
+print('True' or 'True')
+print('True' or 'False')
+print('False' or 'True')
+print('False' or 'False')
+
+#not (complemment)
+
+print(not True)
+print(not False)
