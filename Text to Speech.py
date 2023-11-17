538727bef1da127d548a4b5d21bcae1950dac968
Amit Verma <v99amit@gmail.com>
2023-11-17 18:09:02 +0530

    Commit

---
+++ Text to Speech.py
@@ -0,0 +1,11 @@
+employeeName=str(input("Enter employee name :"))
+employeeId=int(input("Enter employee Id :"))
+employeeAddress=str(input("Enter employee address :"))
+employeeSalary=float(input("Enter employee salary :"))
+
+#print(employeeName)
+#print(employeeId)
+#print(employeeAddress)
+#print(employeeSalary)
+
+print("employee Name =%s employee Id=%d employee Address=%s employee Salary=%g" %,(employeeName,employeeId,employeeAddress,employeeSalary)
