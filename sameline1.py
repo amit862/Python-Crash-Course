5e3d07b35c97aca7ec22528b49b8c24d81a9d5c4
Amit Verma <v99amit@gmail.com>
2023-11-18 18:10:30 +0530

    Commit

---
+++ sameline1.py
@@ -0,0 +1,11 @@
+#multiplication of two number in same line
+a,b=[int(x) for x in input("Enter three number : ").split()]
+print("The product of a and b :",a*b)
+
+#multiplication of three number in same line
+a,b,c=[int(x) for x in input("Enter three number : ").split()]
+print("The product of a and b :",a*b*c)
+
+#multiplication of four number in same line
+a,b,c,d=[float(x) for x in input("Enter three number : ").split(',')]
+print("The product of a and b :",a*b*c*d)
