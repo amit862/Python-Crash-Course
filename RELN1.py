5de113e0abc338b2ff5c7e825eb8307ac4c53cf1
Amit Verma <v99amit@gmail.com>
2023-12-05 22:48:35 +0530

    Commit

---
+++ RELN1.py
@@ -0,0 +1,21 @@
+'''
+a=10
+b=5
+print("a>b is ",a>b)
+print("a>=b is ",a>=b)
+print("a<b is ",a<b)
+print("a<b is ",a<=b)
+
+a='Ranjan'
+b='uday'
+print("a>b is",a>b)
+print("a>=b is",a>=b)
+print("a<b is",a<b)
+print("a<=b is",a<=b)
+'''
+a='ranjan'
+b='ranjan'
+print("a>b is",a>b)
+print("a>=b is",a>b)
+print("a<b is",a<b)
+print("a<=b is",a<=b)
